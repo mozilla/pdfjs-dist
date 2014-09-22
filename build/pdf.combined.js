6e4328d6e69b846467a9a9003eea76562c707727
Yury Delendik <ydelendik@mozilla.com>
2014-09-22 16:23:56 -0500

    PDF.js version 1.0.221

--- build/pdf.combined.js
+++ build/pdf.combined.js
@@ -21,8 +21,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.219';
-PDFJS.build = 'c0419d7';
+PDFJS.version = '1.0.221';
+PDFJS.build = 'b7d8296';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
@@ -10434,7 +10434,7 @@ var Catalog = (function CatalogClosure() {
     },
     get attachments() {
       var xref = this.xref;
-      var attachments, nameTreeRef;
+      var attachments = null, nameTreeRef;
       var obj = this.catDict.get('Names');
       if (obj) {
         nameTreeRef = obj.getRaw('EmbeddedFiles');
--- build/pdf.js
+++ build/pdf.js
@@ -21,8 +21,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.219';
-PDFJS.build = 'c0419d7';
+PDFJS.version = '1.0.221';
+PDFJS.build = 'b7d8296';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
