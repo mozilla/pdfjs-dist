d59ffd01d6484a0640398ca8b726626f0413149c
Yury Delendik <ydelendik@mozilla.com>
2014-09-22 16:29:04 -0500

    PDF.js version 1.0.454

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -21,8 +21,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.452';
-PDFJS.build = '37815ec';
+PDFJS.version = '1.0.454';
+PDFJS.build = 'e97efaa';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
@@ -586,8 +586,15 @@ var IDENTITY_MATRIX = [1, 0, 0, 1, 0, 0];
 var Util = PDFJS.Util = (function UtilClosure() {
   function Util() {}
 
+  var rgbBuf = ['rgb(', 0, ',', 0, ',', 0, ')'];
+
+  // makeCssRgb() can be called thousands of times. Using |rgbBuf| avoids
+  // creating many intermediate strings.
   Util.makeCssRgb = function Util_makeCssRgb(rgb) {
-    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+    rgbBuf[1] = rgb[0];
+    rgbBuf[3] = rgb[1];
+    rgbBuf[5] = rgb[2];
+    return rgbBuf.join('');
   };
 
   // Concatenates two transformation matrices together and returns the result.
