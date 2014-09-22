4b0e98fb69bb6ad1dafae225cfe9742bbcff96b9
Yury Delendik <ydelendik@mozilla.com>
2014-09-22 16:26:30 -0500

    PDF.js version 1.0.347

--- build/pdf.js
+++ build/pdf.js
@@ -21,8 +21,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.345';
-PDFJS.build = '669b513';
+PDFJS.version = '1.0.347';
+PDFJS.build = '06cdca4';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
