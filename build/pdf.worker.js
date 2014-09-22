06c34ccaf38cd19b1e3acfc4b3ddc9d7409d5747
Yury Delendik <ydelendik@mozilla.com>
2014-09-22 16:33:04 -0500

    PDF.js version 1.0.657

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -22,8 +22,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.0.655';
-PDFJS.build = 'c369150';
+PDFJS.version = '1.0.657';
+PDFJS.build = 'f36bfcc';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
@@ -3826,8 +3826,6 @@ var XRef = (function XRefClosure() {
           // Validate entry obj
           if (!isInt(entry.offset) || !isInt(entry.gen) ||
               !(entry.free || entry.uncompressed)) {
-            console.log(entry.offset, entry.gen, entry.free,
-                        entry.uncompressed);
             error('Invalid entry in XRef subsection: ' + first + ', ' + count);
           }
 
