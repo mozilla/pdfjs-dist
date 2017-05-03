9d432c5f903cec467a0498d2f1fb7acc667fcc9f
pdfjsbot <pdfjsbot@gmail.com>
2017-05-03 19:19:04 +0000

    PDF.js version 1.8.310 - See mozilla/pdf.js@e42fc546a0b4b9e8b47f0005610bfcf4ec405ee2

--- build/pdf.js
+++ build/pdf.js
@@ -3455,8 +3455,8 @@ var _UnsupportedManager = function UnsupportedManagerClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '1.8.308';
-  exports.build = build = '74ba3033';
+  exports.version = version = '1.8.310';
+  exports.build = build = 'e42fc546';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -5424,8 +5424,8 @@ if (!_util.globalScope.PDFJS) {
 }
 var PDFJS = _util.globalScope.PDFJS;
 {
-  PDFJS.version = '1.8.308';
-  PDFJS.build = '74ba3033';
+  PDFJS.version = '1.8.310';
+  PDFJS.build = 'e42fc546';
 }
 PDFJS.pdfBug = false;
 if (PDFJS.verbosity !== undefined) {
@@ -7920,8 +7920,8 @@ exports.TilingPattern = TilingPattern;
 "use strict";
 
 
-var pdfjsVersion = '1.8.308';
-var pdfjsBuild = '74ba3033';
+var pdfjsVersion = '1.8.310';
+var pdfjsBuild = 'e42fc546';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(9);
 var pdfjsDisplayAPI = __w_pdfjs_require__(3);
--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -37020,8 +37020,8 @@ exports.Type1Parser = Type1Parser;
 "use strict";
 
 
-var pdfjsVersion = '1.8.308';
-var pdfjsBuild = '74ba3033';
+var pdfjsVersion = '1.8.310';
+var pdfjsBuild = 'e42fc546';
 var pdfjsCoreWorker = __w_pdfjs_require__(8);
 {
   __w_pdfjs_require__(19);
