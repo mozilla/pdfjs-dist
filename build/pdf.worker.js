0d26dcbc7d66b3361ccb634b30f684db78897752
pdfjsbot <pdfjsbot@gmail.com>
2017-08-25 22:34:28 +0000

    PDF.js version 1.9.478 - See mozilla/pdf.js@798e46da975ec55e69c1f7ccf069510c48e2cc70

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -42137,8 +42137,8 @@ exports.Type1Parser = Type1Parser;
 "use strict";
 
 
-var pdfjsVersion = '1.9.476';
-var pdfjsBuild = '26c45369';
+var pdfjsVersion = '1.9.478';
+var pdfjsBuild = '798e46da';
 var pdfjsCoreWorker = __w_pdfjs_require__(61);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
