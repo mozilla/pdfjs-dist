a289ae51572efdbbe47572ba39724d097fa206d9
pdfjsbot <pdfjsbot@gmail.com>
2017-10-10 21:22:36 +0000

    PDF.js version 1.9.640 - See mozilla/pdf.js@853db85b7648397148c4b6ef77852578ee1aa8a1

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -24705,8 +24705,8 @@ exports.PostScriptCompiler = PostScriptCompiler;
 "use strict";
 
 
-var pdfjsVersion = '1.9.638';
-var pdfjsBuild = 'c8023772';
+var pdfjsVersion = '1.9.640';
+var pdfjsBuild = '853db85b';
 var pdfjsCoreWorker = __w_pdfjs_require__(62);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
@@ -24911,7 +24911,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '1.9.638';
+    var workerVersion = '1.9.640';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
