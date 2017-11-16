ac32a73ed8a6c82260d719c0ed48c70b45dc8b1e
pdfjsbot <pdfjsbot@gmail.com>
2017-11-16 22:53:59 +0000

    PDF.js version 2.0.124 - See mozilla/pdf.js@374709a40aaf62a3fe0c5129ec469457cdfebeaf

--- build/pdf.js
+++ build/pdf.js
@@ -3291,7 +3291,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
   if (worker.destroyed) {
     return Promise.reject(new Error('Worker was destroyed'));
   }
-  var apiVersion = '2.0.122';
+  var apiVersion = '2.0.124';
   source.disableAutoFetch = (0, _dom_utils.getDefaultSetting)('disableAutoFetch');
   source.disableStream = (0, _dom_utils.getDefaultSetting)('disableStream');
   source.chunkedViewerLoading = !!pdfDataRangeTransport;
@@ -4654,8 +4654,8 @@ var InternalRenderTask = function InternalRenderTaskClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '2.0.122';
-  exports.build = build = '1d67d9dc';
+  exports.version = version = '2.0.124';
+  exports.build = build = '374709a4';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -7730,8 +7730,8 @@ exports.SVGGraphics = SVGGraphics;
 "use strict";
 
 
-var pdfjsVersion = '2.0.122';
-var pdfjsBuild = '1d67d9dc';
+var pdfjsVersion = '2.0.124';
+var pdfjsBuild = '374709a4';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(113);
 var pdfjsDisplayAPI = __w_pdfjs_require__(57);
@@ -13306,8 +13306,8 @@ if (!_global_scope2.default.PDFJS) {
 }
 var PDFJS = _global_scope2.default.PDFJS;
 {
-  PDFJS.version = '2.0.122';
-  PDFJS.build = '1d67d9dc';
+  PDFJS.version = '2.0.124';
+  PDFJS.build = '374709a4';
 }
 PDFJS.pdfBug = false;
 if (PDFJS.verbosity !== undefined) {
--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -21914,8 +21914,8 @@ exports.PostScriptCompiler = PostScriptCompiler;
 "use strict";
 
 
-var pdfjsVersion = '2.0.122';
-var pdfjsBuild = '1d67d9dc';
+var pdfjsVersion = '2.0.124';
+var pdfjsBuild = '374709a4';
 var pdfjsCoreWorker = __w_pdfjs_require__(71);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
@@ -22120,7 +22120,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.122';
+    var workerVersion = '2.0.124';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
