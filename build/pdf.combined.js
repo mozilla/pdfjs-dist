019c5f89e5071ab618c2084b29ee59d80c010e79
pdfjsbot <pdfjsbot@gmail.com>
2017-11-25 13:39:12 +0000

    PDF.js version 2.0.159 - See mozilla/pdf.js@ada47fe3737867088e756429f9753d47aa2955b8

--- build/pdf.combined.js
+++ build/pdf.combined.js
@@ -11607,7 +11607,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
   if (worker.destroyed) {
     return Promise.reject(new Error('Worker was destroyed'));
   }
-  var apiVersion = '2.0.157';
+  var apiVersion = '2.0.159';
   source.disableRange = (0, _dom_utils.getDefaultSetting)('disableRange');
   source.disableAutoFetch = (0, _dom_utils.getDefaultSetting)('disableAutoFetch');
   source.disableStream = (0, _dom_utils.getDefaultSetting)('disableStream');
@@ -11745,9 +11745,10 @@ var PDFDocumentProxy = function PDFDocumentProxyClosure() {
     get fingerprint() {
       return this.pdfInfo.fingerprint;
     },
-    getPage: function PDFDocumentProxy_getPage(pageNumber) {
+    getPage: function getPage(pageNumber) {
       return this.transport.getPage(pageNumber);
     },
+
     getPageIndex: function PDFDocumentProxy_getPageIndex(ref) {
       return this.transport.getPageIndex(ref);
     },
@@ -12612,7 +12613,7 @@ var WorkerTransport = function WorkerTransportClosure() {
     getData: function WorkerTransport_getData() {
       return this.messageHandler.sendWithPromise('GetData', null);
     },
-    getPage: function WorkerTransport_getPage(pageNumber, capability) {
+    getPage: function getPage(pageNumber) {
       var _this12 = this;
 
       if (!Number.isInteger(pageNumber) || pageNumber <= 0 || pageNumber > this.numPages) {
@@ -12633,6 +12634,7 @@ var WorkerTransport = function WorkerTransportClosure() {
       this.pagePromises[pageIndex] = promise;
       return promise;
     },
+
     getPageIndex: function WorkerTransport_getPageIndexByRef(ref) {
       return this.messageHandler.sendWithPromise('GetPageIndex', { ref: ref }).catch(function (reason) {
         return Promise.reject(new Error(reason));
@@ -12885,8 +12887,8 @@ var InternalRenderTask = function InternalRenderTaskClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '2.0.157';
-  exports.build = build = '343f331b';
+  exports.version = version = '2.0.159';
+  exports.build = build = 'ada47fe3';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -26519,8 +26521,8 @@ exports.SVGGraphics = SVGGraphics;
 "use strict";
 
 
-var pdfjsVersion = '2.0.157';
-var pdfjsBuild = '343f331b';
+var pdfjsVersion = '2.0.159';
+var pdfjsBuild = 'ada47fe3';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(129);
 var pdfjsDisplayAPI = __w_pdfjs_require__(65);
@@ -31849,8 +31851,8 @@ if (!_global_scope2.default.PDFJS) {
 }
 var PDFJS = _global_scope2.default.PDFJS;
 {
-  PDFJS.version = '2.0.157';
-  PDFJS.build = '343f331b';
+  PDFJS.version = '2.0.159';
+  PDFJS.build = 'ada47fe3';
 }
 PDFJS.pdfBug = false;
 if (PDFJS.verbosity !== undefined) {
@@ -35200,7 +35202,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.157';
+    var workerVersion = '2.0.159';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -21883,8 +21883,8 @@ exports.PostScriptCompiler = PostScriptCompiler;
 "use strict";
 
 
-var pdfjsVersion = '2.0.157';
-var pdfjsBuild = '343f331b';
+var pdfjsVersion = '2.0.159';
+var pdfjsBuild = 'ada47fe3';
 var pdfjsCoreWorker = __w_pdfjs_require__(72);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
@@ -22089,7 +22089,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.157';
+    var workerVersion = '2.0.159';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
