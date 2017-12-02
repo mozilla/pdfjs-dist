b1969d3a9942f0d7696d26f61b00b1d498c96373
pdfjsbot <pdfjsbot@gmail.com>
2017-12-02 19:16:03 +0000

    PDF.js version 2.0.181 - See mozilla/pdf.js@98457d9e928f2199a5ff7e3496453e662b7ed85b

--- build/pdf.js
+++ build/pdf.js
@@ -3290,7 +3290,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
   if (worker.destroyed) {
     return Promise.reject(new Error('Worker was destroyed'));
   }
-  var apiVersion = '2.0.179';
+  var apiVersion = '2.0.181';
   source.disableRange = (0, _dom_utils.getDefaultSetting)('disableRange');
   source.disableAutoFetch = (0, _dom_utils.getDefaultSetting)('disableAutoFetch');
   source.disableStream = (0, _dom_utils.getDefaultSetting)('disableStream');
@@ -4651,8 +4651,8 @@ var InternalRenderTask = function InternalRenderTaskClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '2.0.179';
-  exports.build = build = '4894518c';
+  exports.version = version = '2.0.181';
+  exports.build = build = '98457d9e';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -7382,8 +7382,8 @@ exports.SVGGraphics = SVGGraphics;
 "use strict";
 
 
-var pdfjsVersion = '2.0.179';
-var pdfjsBuild = '4894518c';
+var pdfjsVersion = '2.0.181';
+var pdfjsBuild = '98457d9e';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(112);
 var pdfjsDisplayAPI = __w_pdfjs_require__(57);
@@ -12712,8 +12712,8 @@ if (!_global_scope2.default.PDFJS) {
 }
 var PDFJS = _global_scope2.default.PDFJS;
 {
-  PDFJS.version = '2.0.179';
-  PDFJS.build = '4894518c';
+  PDFJS.version = '2.0.181';
+  PDFJS.build = '98457d9e';
 }
 PDFJS.pdfBug = false;
 if (PDFJS.verbosity !== undefined) {
