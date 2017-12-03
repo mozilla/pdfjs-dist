7c7e6ba6aec83f562280dfd48ca20fa10a6e60a7
pdfjsbot <pdfjsbot@gmail.com>
2017-12-03 12:34:50 +0000

    PDF.js version 2.0.183 - See mozilla/pdf.js@de0bac727ebb172d5a87a6eb619006b0eec8f3dc

--- build/pdf.js
+++ build/pdf.js
@@ -3290,7 +3290,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
   if (worker.destroyed) {
     return Promise.reject(new Error('Worker was destroyed'));
   }
-  var apiVersion = '2.0.181';
+  var apiVersion = '2.0.183';
   source.disableRange = (0, _dom_utils.getDefaultSetting)('disableRange');
   source.disableAutoFetch = (0, _dom_utils.getDefaultSetting)('disableAutoFetch');
   source.disableStream = (0, _dom_utils.getDefaultSetting)('disableStream');
@@ -4651,8 +4651,8 @@ var InternalRenderTask = function InternalRenderTaskClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '2.0.181';
-  exports.build = build = '98457d9e';
+  exports.version = version = '2.0.183';
+  exports.build = build = 'de0bac72';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -7382,8 +7382,8 @@ exports.SVGGraphics = SVGGraphics;
 "use strict";
 
 
-var pdfjsVersion = '2.0.181';
-var pdfjsBuild = '98457d9e';
+var pdfjsVersion = '2.0.183';
+var pdfjsBuild = 'de0bac72';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(112);
 var pdfjsDisplayAPI = __w_pdfjs_require__(57);
@@ -12712,8 +12712,8 @@ if (!_global_scope2.default.PDFJS) {
 }
 var PDFJS = _global_scope2.default.PDFJS;
 {
-  PDFJS.version = '2.0.181';
-  PDFJS.build = '98457d9e';
+  PDFJS.version = '2.0.183';
+  PDFJS.build = 'de0bac72';
 }
 PDFJS.pdfBug = false;
 if (PDFJS.verbosity !== undefined) {
--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -21883,8 +21883,8 @@ exports.PostScriptCompiler = PostScriptCompiler;
 "use strict";
 
 
-var pdfjsVersion = '2.0.181';
-var pdfjsBuild = '98457d9e';
+var pdfjsVersion = '2.0.183';
+var pdfjsBuild = 'de0bac72';
 var pdfjsCoreWorker = __w_pdfjs_require__(72);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
@@ -22089,7 +22089,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.181';
+    var workerVersion = '2.0.183';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
