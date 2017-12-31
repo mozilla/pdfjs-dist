5108c841c8e23b26b7d80d2959cd2ab2c0b09428
pdfjsbot <pdfjsbot@gmail.com>
2017-12-31 12:17:08 +0000

    PDF.js version 2.0.241 - See mozilla/pdf.js@3f88bfcda6bc792358fed8ddd3eed07e974ca064

--- build/pdf.combined.js
+++ build/pdf.combined.js
@@ -11645,7 +11645,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
   if (worker.destroyed) {
     return Promise.reject(new Error('Worker was destroyed'));
   }
-  var apiVersion = '2.0.239';
+  var apiVersion = '2.0.241';
   source.disableRange = (0, _dom_utils.getDefaultSetting)('disableRange');
   source.disableAutoFetch = (0, _dom_utils.getDefaultSetting)('disableAutoFetch');
   source.disableStream = (0, _dom_utils.getDefaultSetting)('disableStream');
@@ -12936,8 +12936,8 @@ var InternalRenderTask = function InternalRenderTaskClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '2.0.239';
-  exports.build = build = '4cc0f8c7';
+  exports.version = version = '2.0.241';
+  exports.build = build = '3f88bfcd';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -26719,8 +26719,8 @@ exports.SVGGraphics = SVGGraphics;
 "use strict";
 
 
-var pdfjsVersion = '2.0.239';
-var pdfjsBuild = '4cc0f8c7';
+var pdfjsVersion = '2.0.241';
+var pdfjsBuild = '3f88bfcd';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(131);
 var pdfjsDisplayAPI = __w_pdfjs_require__(65);
@@ -31900,8 +31900,8 @@ if (!_global_scope2.default.PDFJS) {
 }
 var PDFJS = _global_scope2.default.PDFJS;
 {
-  PDFJS.version = '2.0.239';
-  PDFJS.build = '4cc0f8c7';
+  PDFJS.version = '2.0.241';
+  PDFJS.build = '3f88bfcd';
 }
 PDFJS.pdfBug = false;
 if (PDFJS.verbosity !== undefined) {
@@ -35263,7 +35263,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.239';
+    var workerVersion = '2.0.241';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
