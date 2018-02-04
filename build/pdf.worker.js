40f674c05dbc5e4a1a4d0a2c645ecb51f1266214
pdfjsbot <pdfjsbot@gmail.com>
2018-02-04 19:17:58 +0000

    PDF.js version 2.0.328 - See mozilla/pdf.js@4e40a341a1efbcdfd481a0ef68e3a8a7fc1f01dd

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -22307,8 +22307,8 @@ exports.PostScriptCompiler = PostScriptCompiler;
 "use strict";
 
 
-var pdfjsVersion = '2.0.326';
-var pdfjsBuild = '24f96e0f';
+var pdfjsVersion = '2.0.328';
+var pdfjsBuild = '4e40a341';
 var pdfjsCoreWorker = __w_pdfjs_require__(74);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
@@ -22521,7 +22521,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.326';
+    var workerVersion = '2.0.328';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
