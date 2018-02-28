02dd7abe6339a93bbacfd86e85eee3fc74e8a645
pdfjsbot <pdfjsbot@gmail.com>
2018-02-28 22:01:28 +0000

    PDF.js version 2.0.398 - See mozilla/pdf.js@80bf6150d61b048ca0cde17a5036553570eaea45

--- build/pdf.js
+++ build/pdf.js
@@ -3283,7 +3283,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
   if (worker.destroyed) {
     return Promise.reject(new Error('Worker was destroyed'));
   }
-  var apiVersion = '2.0.396';
+  var apiVersion = '2.0.398';
   source.disableRange = (0, _dom_utils.getDefaultSetting)('disableRange');
   source.disableAutoFetch = (0, _dom_utils.getDefaultSetting)('disableAutoFetch');
   source.disableStream = (0, _dom_utils.getDefaultSetting)('disableStream');
@@ -4691,8 +4691,8 @@ var InternalRenderTask = function InternalRenderTaskClosure() {
 }();
 var version, build;
 {
-  exports.version = version = '2.0.396';
-  exports.build = build = 'ae782148';
+  exports.version = version = '2.0.398';
+  exports.build = build = '80bf6150';
 }
 exports.getDocument = getDocument;
 exports.LoopbackPort = LoopbackPort;
@@ -7454,8 +7454,8 @@ exports.SVGGraphics = SVGGraphics;
 "use strict";
 
 
-var pdfjsVersion = '2.0.396';
-var pdfjsBuild = 'ae782148';
+var pdfjsVersion = '2.0.398';
+var pdfjsBuild = '80bf6150';
 var pdfjsSharedUtil = __w_pdfjs_require__(0);
 var pdfjsDisplayGlobal = __w_pdfjs_require__(121);
 var pdfjsDisplayAPI = __w_pdfjs_require__(59);
