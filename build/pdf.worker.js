379bd5bd7215e5cae0a3e298f37bd64af2da5e70
pdfjsbot <pdfjsbot@gmail.com>
2018-03-08 18:28:46 +0000

    PDF.js version 2.0.421 - See mozilla/pdf.js@e0fb18a339fac7f065f9d9165c634d3a895d3ac5

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -22315,8 +22315,8 @@ exports.PostScriptCompiler = PostScriptCompiler;
 "use strict";
 
 
-var pdfjsVersion = '2.0.419';
-var pdfjsBuild = 'c33bf800';
+var pdfjsVersion = '2.0.421';
+var pdfjsBuild = 'e0fb18a3';
 var pdfjsCoreWorker = __w_pdfjs_require__(74);
 exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
 
@@ -22529,7 +22529,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.0.419';
+    var workerVersion = '2.0.421';
     if (apiVersion !== null && apiVersion !== workerVersion) {
       throw new Error('The API version "' + apiVersion + '" does not match ' + ('the Worker version "' + workerVersion + '".'));
     }
--- web/pdf_viewer.js
+++ web/pdf_viewer.js
@@ -3180,8 +3180,8 @@ var _pdf_single_page_viewer = __w_pdfjs_require__(16);
 
 var _pdf_viewer = __w_pdfjs_require__(17);
 
-var pdfjsVersion = '2.0.419';
-var pdfjsBuild = 'c33bf800';
+var pdfjsVersion = '2.0.421';
+var pdfjsBuild = 'e0fb18a3';
 exports.PDFViewer = _pdf_viewer.PDFViewer;
 exports.PDFSinglePageViewer = _pdf_single_page_viewer.PDFSinglePageViewer;
 exports.PDFPageView = _pdf_page_view.PDFPageView;
